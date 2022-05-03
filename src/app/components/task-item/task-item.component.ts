0f0b2c384e76756439c380a92416509f4f0e3b04
Mohamad <arno41656@gmail.com>
2022-05-03 23:41:52 +0200

    toggle reminder

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -11,11 +11,15 @@ export class TaskItemComponent implements OnInit {
     faTimes = faTimes;
     @Input() task: Task;
     @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+    @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
     constructor() {}
 
     ngOnInit(): void {}
 
     onDelete(task: Task) {
         this.onDeleteTask.emit(task);
+    }
+    onToggle(task: Task) {
+        this.onToggleReminder.emit(task);
     }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,11 @@ import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
 
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+    }),
+};
 @Injectable({
     providedIn: 'root',
 })
@@ -19,5 +24,10 @@ export class TaskService {
     deleteTask(task: Task): Observable<Task> {
         const url = `${this.apiUrl}/${task.id}`;
         return this.http.delete<Task>(url);
+    }
+
+    updateTaskReminder(task: Task): Observable<Task> {
+        const url = `${this.apiUrl}/${task.id}`;
+        return this.http.patch<Task>(url, task, httpOptions);
     }
 }
