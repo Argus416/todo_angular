92351e049646296b0dbb30f986a990ed709e82b7
Mohamad Al khatib <mohamad@alkhatib.com>
2022-05-04 11:41:13 +0200

    header condition view

--- src/app/partiels/header/header.component.ts
+++ src/app/partiels/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
-
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -12,7 +12,7 @@ export class HeaderComponent implements OnInit {
     showAddTask: boolean = true;
     subscription: Subscription;
 
-    constructor(private uiSerivce: UiService) {
+    constructor(private uiSerivce: UiService, private router: Router) {
         this.subscription = this.uiSerivce
             .onToggle()
             .subscribe((Value) => (this.showAddTask = Value));
@@ -20,6 +20,10 @@ export class HeaderComponent implements OnInit {
 
     toggleAddTask() {
         this.uiSerivce.toggleAddTask();
+    }
+
+    hasRoute(route: string) {
+        return this.router.url === route;
     }
     ngOnInit(): void {}
 }
